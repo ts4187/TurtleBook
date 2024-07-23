91faa033b9ee827911fd9a66584ab0304c87a23b
teak seung - kim <119774376+ts4187@users.noreply.github.com>
2024-07-23 16:14:45 +0900

    appconatiner가 로드 되는 시간을 지연시킴

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,6 @@
-<script setup>
-import { ref } from 'vue' // ref = 반응형 상태를 만들기 위해 사용
-import { RouterLink, RouterView } from 'vue-router'
-import plusImage from '/icons/Plus.png' // 이미지 경로
-
-// 반응형 상태 = showLInks / 초기값을 false로 설정 / 객체 형태로 저장, showLinks.value에 저장
-const showLinks = ref(false)
-
-// mouseover가 실행될 경우 showLinks.value의 값을 ture로 변경
-const handleMouseOver = () => {
-  showLinks.value = true
-}
-// mpuseout이 실행될 경우 showLinks.value의 값을 false로 변경
-const handleMouseOut = () => {
-  showLinks.value = false
-}
-</script>
-
 <template>
   <header>
-    <div class="app_container">
+    <div :class="['app_container', { 'fade-in': roadappcontainer }]">
       <div class="wrapper">
         <RouterLink class="openBook_img_RouterLink" to="/">
           <img class="openBook_img" src="/icons/openBook.png" alt="Book" />
@@ -47,10 +29,39 @@ const handleMouseOut = () => {
   </main>
 </template>
 
+<script setup>
+import { onMounted, ref } from 'vue' // ref = 반응형 상태를 만들기 위해 사용
+import { RouterLink, RouterView } from 'vue-router'
+import plusImage from '/icons/Plus.png' // 이미지 경로
+
+const roadappcontainer = ref(false)
+// 반응형 상태 = showLInks / 초기값을 false로 설정 / 객체 형태로 저장, showLinks.value에 저장
+const showLinks = ref(false)
+
+// mouseover가 실행될 경우 showLinks.value의 값을 ture로 변경
+const handleMouseOver = () => {
+  showLinks.value = true
+}
+// mpuseout이 실행될 경우 showLinks.value의 값을 false로 변경
+const handleMouseOut = () => {
+  showLinks.value = false
+}
+
+onMounted (() => {
+  setTimeout(() => {
+    roadappcontainer.value = true
+  }, 2000)
+})
+</script>
+
 <style scoped>
 * {
   margin: 0;
   padding: 0;
+}
+
+.fade-in {
+  opacity: 1;
 }
 
 .wrapper {
@@ -86,6 +97,7 @@ Header {
   justify-content: space-between;
   align-items: center;
   padding: 0 1rem;
+  opacity: 0;
 }
 
 .image-container {
