9d75b6bc6a52d49ac64b653d583524c183a8d9c2
teak seung - kim <119774376+ts4187@users.noreply.github.com>
2024-08-14 15:37:46 +0900

    반응형 페이지 시도 2
    app.vue 에 추가했던 것을 index.html 에 위치 변경

--- src/App.vue
+++ src/App.vue
@@ -36,14 +36,14 @@ import { RouterLink, RouterView } from 'vue-router'
 import plusImage from '/icons/Plus.png' // 이미지 경로
 
 const roadappcontainer = ref(false)
-// 반응형 상태 = showLinks / 초기값을 false로 설정 / 객체 형태로 저장, showLinks.value에 저장
+// 반응형 상태 = showLInks / 초기값을 false로 설정 / 객체 형태로 저장, showLinks.value에 저장
 const showLinks = ref(false)
 
 // mouseover가 실행될 경우 showLinks.value의 값을 ture로 변경
 const handleMouseOver = () => {
   showLinks.value = true
 }
-// mouseout이 실행될 경우 showLinks.value의 값을 false로 변경
+// mpuseout이 실행될 경우 showLinks.value의 값을 false로 변경
 const handleMouseOut = () => {
   showLinks.value = false
 }
@@ -61,10 +61,8 @@ onMounted (() => {
   padding: 0;
 }
 
-/* Base styles */
 .app_container {
   opacity: 0;
-  transition: opacity 2s ease-in;
 }
 
 .fade-in {
@@ -82,10 +80,9 @@ onMounted (() => {
   height: 60px;
 }
 
-header {
+Header {
   width: 100%;
   position: fixed;
-  top: 0;
 }
 
 .site_name {
@@ -142,102 +139,5 @@ header {
 
 .RouterLink:hover {
   text-decoration: underline;
-}
-
-/* Responsive Styles */
-
-/* Mobile (Small devices, 600px and down) */
-@media only screen and (max-width: 600px) {
-  .wrapper {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .site_name {
-    font-size: 18px;
-  }
-
-  .openBook_img {
-    width: 50px;
-    height: 50px;
-  }
-
-  .plus-icon {
-    width: 28px;
-    height: 28px;
-  }
-
-  .hidden-nav {
-    left: 0; /* 중앙 정렬 */
-    width: 100vw; /* 화면 전체 너비 사용 */
-    text-align: center;
-  }
-
-  .RouterLink {
-    padding: 1rem;
-  }
-}
-
-/* Tablets (601px to 768px) */
-@media only screen and (min-width: 601px) and (max-width: 768px) {
-  .wrapper {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .site_name {
-    font-size: 20px;
-  }
-
-  .openBook_img {
-    width: 55px;
-    height: 55px;
-  }
-
-  .plus-icon {
-    width: 30px;
-    height: 30px;
-  }
-
-  .hidden-nav {
-    left: -40px; /* 살짝 왼쪽으로 이동 */
-  }
-
-  .RouterLink {
-    padding: 0.75rem 1.5rem;
-  }
-}
-
-/* Desktop (769px and up) */
-@media only screen and (min-width: 769px) {
-  .wrapper {
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .site_name {
-    font-size: 23px;
-  }
-
-  .openBook_img {
-    width: 60px;
-    height: 60px;
-  }
-
-  .plus-icon {
-    width: 32px;
-    height: 32px;
-  }
-
-  .hidden-nav {
-    left: -80px; /* 왼쪽으로 이동 */
-  }
-
-  .RouterLink {
-    padding: 0.5rem 1rem;
-  }
 }
 </style>
